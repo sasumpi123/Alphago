020e524ec9bdfc7a3158e95e43fa3c4c0e6a207e
sasumpi123 <>
2020-09-06 21:22:43 +0900

    이진트리

--- 8.30~9.6/binaryTree.js
+++ 8.30~9.6/binaryTree.js
@@ -1,17 +1,5 @@
-/**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
-/**
- * @param {TreeNode} root
- * @return {number}
- */
-
-
+// Runtime    Memory
+// 88ms       40.2MB
 
 var diameterOfBinaryTree = function(root) {
     let answer = 0;
