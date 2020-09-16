8ea77e59382a9d3e9611ac103200f6d2aa865c97
sasumpi123 <>
2020-09-16 21:31:47 +0900

    6번문제 저풀이보고 뇌가정지함

--- 9.7~9.14/strangeCounter.js
+++ 9.7~9.14/strangeCounter.js
@@ -1,30 +1,13 @@
-function strangeCounter(t) {    
-    let index = 0;
-    let time = 1;
+// 현타씨게옴
+// 이수진갓;
+function strangeCounter(t) {
+  let time = 3;
+  let num = 6;
 
-    while(true){        
-        if(index==0){
-            time = 1;
-        }else{
-            time = time + Math.pow(2, index-1)*3;
-        }
-        if(time>=t)break;
-        index++;
-    }
-    let value = 0;
-    if(time==t){
-        value = 3*Math.pow(2,index);  
-        return value;
-    } else{
-        value = 3*Math.pow(2,index-1);  
-    }
-    for(let i = time - Math.pow(2, index-1)*3; i<time; i++){
-        if(i==t){
-            return value;
-        }
-        value--;
-    }
+  while (t > time) {
+    time += num;
+    num *= 2;
+  }
+
+  return time - t + 1;
 }
-
-
-strangeCounter(17);
