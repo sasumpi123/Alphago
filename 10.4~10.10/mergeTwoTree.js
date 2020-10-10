3d9302efd7a46c9f2a346df7c5a8c7eab7231e13
sasumpi123 <sasumpi123@naver.com>
2020-10-10 19:51:54 +0900

    트리합치기

---
+++ 10.4~10.10/mergeTwoTree.js
@@ -0,0 +1,13 @@
+// Runtime      Memory
+// 100 ms	     46.3 MB
+
+var mergeTrees = function (t1, t2) {
+  if (!t1) return t2;
+  if (!t2) return t1;
+
+  t1.val += t2.val;
+  t1.left = mergeTrees(t1.left, t2.left);
+  t1.right = mergeTrees(t1.right, t2.right);
+
+  return t1;
+};
