4dd749a6b4e84f0c2fc7a01c7248644d9bfcee73
sasumpi123 <sasumpi123@naver.com>
2021-07-12 19:04:17 +0900

    재귀연습 (토마토는 거꾸로해도 토마토~)

--- practice/receive.js
+++ practice/receive.js
@@ -8,3 +8,21 @@ function factorial(n) {
 }
 
 factorial(5);
+
+// 문자열이 거꾸로 읽어도 동일한지 여부를 재귀적으로 결정하는 프로그램 작성하기
+function isPalindrome(str) {
+  return palindrome(str, 0, str.length - 1);
+}
+
+function palindrome(str, startIdx, endIdx) {
+  const isEnd = startIdx >= endIdx;
+  if (isEnd) return true;
+
+  if (str.charAt(startIdx) !== str.charAt(endIdx)) {
+    return false;
+  } else {
+    return palindrome(str, startIdx + 1, endIdx - 1);
+  }
+}
+
+console.error("accca : ", isPalindrome("accca"));
